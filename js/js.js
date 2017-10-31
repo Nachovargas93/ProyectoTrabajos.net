3590735fb130e17a35402e1c69b555e6a04ff3bf
Nachovargas93 <nachovargas93@gmail.com>
2017-10-31 10:27:00 -0300

    primerCommit

---
+++ js/js.js
@@ -0,0 +1,56 @@
+var config = {
+        apiKey: "AIzaSyBmFKnM5JQIKgHUz7aB2eUbXqpeAXg0DWY",
+        authDomain: "trabajosnet01.firebaseapp.com",
+        databaseURL: "https://trabajosnet01.firebaseio.com",
+        projectId: "trabajosnet01",
+        storageBucket: "trabajosnet01.appspot.com",
+        messagingSenderId: "519327363611"
+    }
+
+function cargar(carga) {
+	$('#container').load(carga + ".html");
+}
+
+function register() {
+	var user = {
+		"nombre": "",
+		"apellido": "",
+		"edad": "",
+		"email": "",
+		"telefono": "",
+		"experiencias": "",
+		"password": ""
+	}
+
+	user.nombre = $("#txt_nombre").val();
+	user.apellido = $("txt_apellido").val();
+	user.edad = $("#txt_edad").val();
+	user.email = $("#txt_email").val();
+	user.telefono = $("#txt_telefono").val();
+	user.experiencias = $("#txt_experiencias").val();
+	user.password = $("#txt_password").val();
+
+	firebase.auth().createUserWithEmailAndPassword(user.email, user.password).catch(function(error) {
+		console.log(error.code);
+		console.log(error.message);
+	});
+}
+
+function login() {
+	var email = $('#txt_email').val();
+	var password = $('#txt_password').val();
+
+	var user = firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+		console.log(error.code);
+		console.log(error.message);
+	});
+
+	console.log(user);
+	cargar('personas');
+}
+
+$(document).ready(function() {
+	cargar("login");
+	firebase.initializeApp(config);
+});
+	
