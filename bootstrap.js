6038bab07465b1eb7a7c9e49465cafac00dfd0dc
Nachovargas93 <nachovargas93@gmail.com>
2017-11-07 08:48:15 -0300

    Base de datos casi lista

---
+++ bootstrap.js
@@ -0,0 +1,15 @@
+# TrabajosNet
+***
+**Alumno**
+ : Vargas Ignacio
+
+**Curso**
+ : 5to Año "C" ITS Villada
+ 
+ **Asignatura**
+ : Programacion 2
+
+# Descripcion
+***
+En _TrabajosNet_ podras subir tu curriculum vitae para que empresas, personas,etc puedan saber mas de ti y asi contratarte.
+Esto es muy facil de utilizar, solo debes registrarte y cargar tus datos, 
--- js/js.js
+++ js/js.js
@@ -5,13 +5,23 @@ var config = {
         projectId: "trabajosnet01",
         storageBucket: "trabajosnet01.appspot.com",
         messagingSenderId: "519327363611"
-    }
+    };
 
 function cargar(carga) {
 	$('#container').load(carga + ".html");
 }
 
 function register() {
+	var nombre = $("#txt_nombre").val();
+	var apellido = $("txt_apellido").val();
+	var edad = $("#txt_edad").val();
+	var email = $("#txt_email").val();
+	var telefono = $("#txt_telefono").val();
+	var experiencias = $("#txt_experiencias").val();
+	var password = $("#txt_password").val();
+
+
+
 	var user = {
 		"nombre": "",
 		"apellido": "",
@@ -20,20 +30,34 @@ function register() {
 		"telefono": "",
 		"experiencias": "",
 		"password": ""
-	}
+	};
 
 	user.nombre = $("#txt_nombre").val();
-	user.apellido = $("txt_apellido").val();
+	user.apellido = $("#txt_apellido").val();
 	user.edad = $("#txt_edad").val();
 	user.email = $("#txt_email").val();
 	user.telefono = $("#txt_telefono").val();
 	user.experiencias = $("#txt_experiencias").val();
 	user.password = $("#txt_password").val();
 
+	var db = firebase.database();
+
+	db.ref("usuarios").push({
+		'nombre': nombre,
+		'apellido': apellido,
+		'edad': edad,
+		'email': email,
+		'telefono': telefono,
+		'experiencias': experiencias,
+		'password': password
+	});
+
 	firebase.auth().createUserWithEmailAndPassword(user.email, user.password).catch(function(error) {
 		console.log(error.code);
 		console.log(error.message);
 	});
+
+
 }
 
 function login() {
@@ -47,6 +71,10 @@ function login() {
 
 	console.log(user);
 	cargar('personas');
+}
+
+function leerUsuarios() {
+	databaseUsuarios = firebase.database().ref("/user")
 }
 
 $(document).ready(function() {
